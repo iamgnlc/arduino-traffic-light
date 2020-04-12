98f665a18cdd1f5d50dae463fb1523a04a4cc60c
GNLC <gianluca.agnocchetti@gmail.com>
2020-04-12 16:58:20 +0100

    extracted logic into class

--- TrafficLight.js
+++ TrafficLight.js
@@ -1,59 +1,82 @@
 const { Led } = require("johnny-five");
 
 class TrafficLight {
-  constructor(transition = 1000, blink = 450) {
+  constructor(interval, transition = 1000, blink = 450) {
+    this.interval = interval;
     this.transition = transition;
     this.blink = blink;
+    this.nextState;
     this.leds = {
       green: new Led(11),
       yellow: new Led(12),
       red: new Led(13),
     };
   }
-  // Lights off.
-  allOff() {
-    this.leds.green.stop().off();
-    this.leds.yellow.stop().off();
-    this.leds.red.stop().off();
-  }
   // Turn on red.
-  onlyRed() {
+  _onlyRed() {
     this.leds.green.off();
     this.leds.yellow.stop().off();
     this.leds.red.on();
   }
   // Turn on yellow.
-  onlyYellow() {
+  _onlyYellow() {
     this.leds.green.off();
     this.leds.yellow.stop().on();
     this.leds.red.off();
   }
   // Blink yellow.
-  blinkYellow() {
+  _blinkYellow() {
     this.leds.green.off();
     this.leds.yellow.blink(this.blink);
     this.leds.red.off();
   }
   // Turn on green.
-  onlyGreen() {
+  _onlyGreen() {
     this.leds.green.on();
     this.leds.yellow.stop().off();
     this.leds.red.off();
   }
   // Transition to red.
-  toRed() {
-    this.onlyYellow();
+  _toRed() {
+    this._onlyYellow();
     setTimeout(() => {
-      this.onlyRed();
+      this._onlyRed();
     }, this.transition);
   }
   // Transition to green.
-  toGreen() {
+  _toGreen() {
     this.leds.red.on();
     this.leds.yellow.stop().on();
     setTimeout(() => {
-      this.onlyGreen();
+      this._onlyGreen();
     }, this.transition);
+  }
+  // Lights off.
+  reset() {
+    this.leds.green.off();
+    this.leds.yellow.stop().off();
+    this.leds.red.off();
+  }
+  start() {
+    // Init.
+    this.reset();
+    this._blinkYellow();
+    setInterval(() => {
+      switch (this.nextState) {
+        default:
+        case "red":
+          this._toGreen();
+          this.nextState = "green";
+          break;
+        case "green":
+          this._toRed();
+          this.nextState = "red";
+          break;
+      }
+    }, this.interval);
+  }
+  destructor() {
+    this.reset();
   }
 }
 
--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const { Board } = require("johnny-five");
 
-const board = new Board();
-
 const TrafficLight = require("./TrafficLight");
+
+const board = new Board();
 
 // Ready.
 board
   .on("ready", () => {
-    let light;
     const interval = 5000;
     const transition = 1500;
-    const trafficLight = new TrafficLight(transition);
+    const trafficLight = new TrafficLight(interval, transition);
 
     // Init.
-    trafficLight.allOff();
-    trafficLight.blinkYellow();
-    setInterval(() => {
-      switch (light) {
-        default:
-        case "red":
-          trafficLight.toGreen();
-          light = "green";
-          break;
-        case "green":
-          trafficLight.toRed();
-          light = "red";
-          break;
-      }
-    }, interval);
+    trafficLight.start();
   })
   // Exit.
   .on("exit", () => {
     const trafficLight = new TrafficLight();
-    trafficLight.allOff();
+    trafficLight.reset();
   });
